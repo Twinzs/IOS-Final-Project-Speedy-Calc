a940c29c5d34fde366fe54a080c53c1dd40ba8b8
Jonathan Lace <jdlace@icloud.com>
2022-03-22 09:23:19 -0400

    Initial commit

--- TempCalculator/ViewController.swift
+++ TempCalculator/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    
+    @IBOutlet weak var cToFtextField: UITextField!
+    
+    @IBOutlet weak var fToCtextField: UITextField!
+    
 
+    
+    @IBAction func convertToF(_ sender: Any) {
+        
+        let numberString = cToFtextField.text!
+        let number = Double(numberString)
+        
+        let fahrenheitResult = (number!) * 1.8 + 32
+        let roundedFahrenheit = round(fahrenheitResult * 10) / 10
+        
+        fahrenheitLabel.text = "\(numberString) degrees Celsius is \(String(roundedFahrenheit)) degrees Fahrenheit."
+        
+    }
+    
+    
+    @IBAction func convertToC(_ sender: Any) {
+        
+        let numberString = fToCtextField.text!
+        let number = Double(numberString)
+        
+        let celsiusResult = (number! - 32) * 5/9
+        let roundedCelsius = round(celsiusResult * 10) / 10
+        
+        celsiusLabel.text = "\(numberString) degrees Fahrenheit is \(String(roundedCelsius)) degrees Celsius."
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        cToFtextField.delegate = self
+        fToCtextField.delegate = self
+        
     }
+    
 
 
+   func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        fahrenheitLabel.text = ""
+        celsiusLabel.text = ""
+       
+       return true
+    }
+ 
+    
 }
 
